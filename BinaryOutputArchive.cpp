694b5ac578ff19ebd2dc06c338044d8beb1917ad
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-07-15 21:12:02 -0700

    Add function to serialize and save data to output stream

--- BinaryOutputArchive.cpp
+++ BinaryOutputArchive.cpp
@@ -2,8 +2,10 @@
 
 namespace draft {
 
-BinaryOutputArchive::BinaryOutputArchive(std::ostream &ostrm) {
+BinaryOutputArchive::BinaryOutputArchive(std::ostream& ostrm) : ostrm_(ostrm) {}
 
+void BinaryOutputArchive::Save(const void* addr, std::size_t size) {
+  ostrm_.write(reinterpret_cast<const char*>(addr), size);
 }
 
 }  // namespace draft
--- BinaryOutputArchive.hpp
+++ BinaryOutputArchive.hpp
@@ -1,31 +1,40 @@
 #include <fstream>
-#include <iostream>
+#include <memory>
 
 namespace draft {
 
 class BinaryOutputArchive {
  public:
-  BinaryOutputArchive(std::ostream &ostrm);
+  explicit BinaryOutputArchive(std::ostream& ostrm);
 
   // Overload the function call operator. This allows clients to use instances
-  // of this class as a function.
+  // of this class as a function. For example:
+  //   draft::BinaryOutputArchive oa(<some output stream>);
+  //   oa("hello", 32);
   template <typename T, typename... ArgTypes>
   void operator()(T t, ArgTypes... args) {
-    process(t, args...);
+    Process(t, args...);
   }
 
  private:
+  // Process and serialize a single element.
   template <typename T>
-  void process(T first) {
-    std::cout << first << std::endl;
+  void Process(T first) {
+    Save(std::addressof(first), sizeof(first));
   }
 
+  // Use variadic template to accept an arbitrary number of arguments. This
+  // allows the user to pass any number of elements to be serialized.
   template <typename T, typename... ArgTypes>
-  void process(T first, ArgTypes... rest) {
-    std::cout << first << std::endl;
-
-    process(rest...);
+  void Process(T first, ArgTypes... rest) {
+    Process(first);
+    Process(rest...);
   }
+
+  // Write the given data to the output stream.
+  void Save(const void* data, std::size_t size);
+
+  std::ostream& ostrm_;
 };
 
 }  // namespace draft
