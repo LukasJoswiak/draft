0e06485b4c0d05b588df1ce2001afbc0fa08269b
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-07-17 21:14:07 -0700

    Add function to serialize arithmetic types

--- BinaryOutputArchive.cpp
+++ BinaryOutputArchive.cpp
@@ -4,7 +4,7 @@ namespace draft {
 
 BinaryOutputArchive::BinaryOutputArchive(std::ostream& ostrm) : ostrm_(ostrm) {}
 
-void BinaryOutputArchive::Save(const void* addr, std::size_t size) {
+void BinaryOutputArchive::SaveToBinary(const void* addr, std::size_t size) const {
   ostrm_.write(reinterpret_cast<const char*>(addr), size);
 }
 
--- BinaryOutputArchive.hpp
+++ BinaryOutputArchive.hpp
@@ -14,31 +14,39 @@ class BinaryOutputArchive {
   // of this class as a function. For example:
   //   draft::BinaryOutputArchive oa(<some output stream>);
   //   oa("hello", 32);
-  template <typename T, typename... ArgTypes>
+  template<typename T, typename... ArgTypes>
   void operator()(T t, ArgTypes... args) {
     Process(t, args...);
   }
 
+  // Write the given data to the output stream.
+  void SaveToBinary(const void* data, std::size_t size) const;
+
  private:
   // Process and serialize a single element.
-  template <typename T>
+  template<typename T>
   void Process(T first) {
-    Save(std::addressof(first), sizeof(first));
+    Save(*this, first);
   }
 
   // Use variadic template to accept an arbitrary number of arguments. This
   // allows the user to pass any number of elements to be serialized.
-  template <typename T, typename... ArgTypes>
+  template<typename T, typename... ArgTypes>
   void Process(T first, ArgTypes... rest) {
     Process(first);
     Process(rest...);
   }
 
-  // Write the given data to the output stream.
-  void Save(const void* data, std::size_t size);
-
   std::ostream& ostrm_;
 };
+
+// The compiler will generate a function with the appropriate type if T
+// passes the is_arithmetic check, i.e. T is an int or float.
+template<typename T>
+typename std::enable_if<std::is_arithmetic<T>::value>::type
+Save(const BinaryOutputArchive& oa, const T& t) {
+  oa.SaveToBinary(std::addressof(t), sizeof(t));
+}
 
 }  // namespace draft
 
