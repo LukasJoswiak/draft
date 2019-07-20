8c64b8f7a2675872090dfd7db61372192817605f
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-07-19 21:16:26 -0700

    Add deserialization class and deserialization for arithmetic types

--- include/binary.hpp
+++ include/binary.hpp
@@ -15,8 +15,13 @@ class BinaryOutputArchive {
   // of this class as a function. For example:
   //   draft::BinaryOutputArchive oa(<some output stream>);
   //   oa("hello", 32);
+  //
+  // It's important to accept rvalue references to maximize flexibility.
+  // This allows calls of the format:
+  //   std::string str("hello");
+  //   oa(str.size());
   template<typename T, typename... ArgTypes>
-  void operator()(T t, ArgTypes... args) {
+  void operator()(T&& t, ArgTypes&&... args) {
     Process(t, args...);
   }
 
@@ -28,19 +33,52 @@ class BinaryOutputArchive {
  private:
   // Process and serialize a single element.
   template<typename T>
-  void Process(T first) {
+  void Process(T&& first) {
     Save(*this, first);
   }
 
   // Use variadic template to accept an arbitrary number of arguments. This
   // allows the user to pass any number of elements to be serialized.
   template<typename T, typename... ArgTypes>
-  void Process(T first, ArgTypes... rest) {
+  void Process(T&& first, ArgTypes&... rest) {
     Process(first);
     Process(rest...);
   }
 
   std::ostream& ostrm_;
+};
+
+class BinaryInputArchive {
+ public:
+  explicit BinaryInputArchive(std::istream& istrm) : istrm_(istrm) {}
+
+  // Overload the function call operator. This allows clients to
+  // pass an arbitrary number of parameters to be populated with
+  // data based on the archived data.
+  template<typename... ArgTypes>
+  void operator()(ArgTypes&&... args) {
+    Process(args...);
+  }
+
+  // Read `size` characters from the archive and store them in the
+  // location `addr` points to.
+  void LoadBinary(void* addr, std::size_t size) const {
+    istrm_.read(reinterpret_cast<char*>(addr), size);
+  }
+
+ private:
+  template<typename T>
+  void Process(T&& first) {
+    Load(*this, first);
+  }
+
+  template<typename T, typename... ArgTypes>
+  void Process(T&& first, ArgTypes&&... rest) {
+    Process(first);
+    Process(rest...);
+  }
+
+  std::istream& istrm_;
 };
 
 // The compiler will generate a function with the appropriate type if T
@@ -49,6 +87,13 @@ template<typename T>
 typename std::enable_if<std::is_arithmetic<T>::value>::type
 Save(const BinaryOutputArchive& oa, const T& t) {
   oa.SaveToBinary(std::addressof(t), sizeof(t));
+}
+
+// Deserialze arithmetic types.
+template<typename T>
+typename std::enable_if<std::is_arithmetic<T>::value>::type
+Load(const BinaryInputArchive& ia, T& t) {
+  ia.LoadBinary(std::addressof(t), sizeof(t));
 }
 
 }  // namespace draft
--- main.cpp
+++ main.cpp
@@ -8,11 +8,21 @@ int main() {
     std::ofstream stream("out", std::ios::binary);
     draft::BinaryOutputArchive oa(stream);
 
-    // oa(32);
-    oa("hello");
+    oa(32);
+    // oa("hello");
+    // oa(std::string("hi"));
 
     // Try running `xxd -b out` on the command line to view the binary
     // dump of `out`.
+  }
+
+  {
+    std::ifstream stream("out", std::ios::binary);
+    draft::BinaryInputArchive ia(stream);
+
+    int a;
+    ia(a);
+    std::cout << "a = " << a << std::endl;
   }
 
   return 0;
