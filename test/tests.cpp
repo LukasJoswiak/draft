282bf2cf7cf5b8ac60b75d6f6b87800cbd064c13
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-07-26 22:23:06 -0700

    Draft -> draft

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,7 +1,7 @@
-// Since Draft is a header-only library, including Draft in each test
+// Since draft is a header-only library, including draft in each test
 // file causes a linker error when combining each test's object file.
 // The solution is to include each test in a single file and compile
-// this file, which prevents multiple includes of the Draft library.
+// this file, which prevents multiple includes of the draft library.
 // This is not a great solution, because each time any test is changed,
 // all tests have to be recompiled, but it is an OK solution for now.
 #include "basic_types.hpp"
