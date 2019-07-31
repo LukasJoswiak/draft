98873bf2412ca6c7c9cbb7e1d520bf93935051a9
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-07-31 08:08:40 -0700

    Add unit tests for bool, char, float

--- test/basic_types.hpp
+++ test/basic_types.hpp
@@ -26,15 +26,42 @@ TEST_CASE("basic types can be serialized") {
     REQUIRE(y == 2147483647);
   }
 
+  SECTION("encode boolean") {
+    bool x = true;
+    oa(x);
+    bool y;
+    ia(y);
+
+    REQUIRE(y == true);
+  }
+
+  SECTION("encode char") {
+    char x = 'd';
+    oa(x);
+    char y;
+    ia(y);
+
+    REQUIRE(y == 'd');
+  }
+
   SECTION("encode floating point value") {
-    double x = 3.14;
+    float x = 3.14;
+    oa(x);
+    float y;
+    ia(y);
+
+    // Equality of floating point should work because the encoding
+    // of the original float and the deserialized float are
+    // exactly the same.
+    REQUIRE(y == 3.14f);
+  }
+
+  SECTION("encode double floating point value") {
+    double x = 1.0 / 31;
     oa(x);
     double y;
     ia(y);
 
-    // Equality of floating point should work because the encoding
-    // of the original double and the deserialized double are
-    // exactly the same.
-    REQUIRE(y == 3.14);
+    REQUIRE(y == 1.0 / 31);
   }
 }
