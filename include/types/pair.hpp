78b73adb329a0da61a2a10ad199f83dd75cd2513
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-08-01 18:23:32 -0700

    Add support for std::pair serialization

--- include/types/pair.hpp
+++ include/types/pair.hpp
@@ -1,4 +1,9 @@
-// TODO: comment
+// std::pair is encoded by encoding its first element followed by encoding
+// its second element. Type information must be provided to determine how
+// many bytes to write when serializing and how many bytes to read when
+// deserializing.
+//
+// Both types of the pair must be serializable to serialize the pair.
 
 #ifndef INCLUDE_PAIR_HPP_
 #define INCLUDE_PAIR_HPP_
@@ -9,16 +14,21 @@
 namespace draft {
 namespace binary {
 
-// TODO: comment
+// Save the pair by writing its elements. Both elements must be
+// serializable.
 template<typename T1, typename T2>
 void Save(OutputArchive&oa, std::pair<T1, T2>& pair) {
-  // TODO: implement
+  oa(pair.first);
+  oa(pair.second);
 }
 
-// TODO: comment
+// Load the pair from the given binary input archive into pair. Type info
+// must be provided to determine how many bytes of the input archive to
+// read.
 template<typename T1, typename T2>
 void Load(binary::InputArchive& ia, std::pair<T1, T2>& pair) {
-  // TODO: implement
+  ia(pair.first);
+  ia(pair.second);
 }
 
 }  // namespace binary
