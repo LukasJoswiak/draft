b07de5cc1ce193d35d76c00aa4cbb76fcb2c07c9
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-08-25 11:44:07 -0700

    Finish serialization implementation for std::unordered_map

--- include/types/unordered_map.hpp
+++ include/types/unordered_map.hpp
@@ -15,14 +15,32 @@ namespace binary {
 // then writing each key-value pair in order.
 template<typename K, typename V>
 void Save(OutputArchive& oa, const std::unordered_map<K, V>& map) {
-  // TODO: implement
+  oa(map.size());
+  for (const auto& kv : map) {
+    oa(kv.first);
+    oa(kv.second);
+  }
 }
 
 // Deserialize the unordered_map by reading the number of elements stored,
 // then reading each key-value pair.
 template<typename K, typename V>
 void Load(binary::InputArchive& ia, std::unordered_map<K, V>& map) {
-  // TODO: implement
+  // Read in size of map.
+  std::size_t size;
+  ia(size);
+
+  for (int i = 0; i < size; ++i) {
+    K key;
+    V value;
+    ia(key);
+    ia(value);
+
+    // Let the map take control of `key` and `value` to avoid having to make
+    // copies.
+    // TODO: This can likely be improved by using emplace_hint instead.
+    map.emplace(std::move(key), std::move(value));
+  }
 }
 
 }  // namespace binary
