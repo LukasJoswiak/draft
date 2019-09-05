b0ee7f976f9026507dc2a5b80d209ef105eb6b88
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-09-04 19:46:59 -0700

    Add unit test for class with an object member

--- test/basic_class.hpp
+++ test/basic_class.hpp
@@ -9,7 +9,7 @@ class Point {
   Point(int x, int y) : x_(x), y_(y) {}
 
   template<typename Archive>
-  void Serialize(Archive& ar) const {
+  void Serialize(Archive& ar) {
     ar(x_, y_);
   }
 
@@ -17,7 +17,7 @@ class Point {
   int y_;
 };
 
-TEST_CASE("basic class can be serialized") {
+TEST_CASE("class with arithmetic types can be serialized") {
   std::stringstream ss;
   draft::binary::OutputArchive oa(ss);
   draft::binary::InputArchive ia(ss);
@@ -30,5 +30,35 @@ TEST_CASE("basic class can be serialized") {
 
     REQUIRE(p2.x_ == 1);
     REQUIRE(p2.y_ == 2);
+  }
+}
+
+class Person {
+ public:
+  Person() {}
+  Person(std::string name, int age) : name_(name), age_(age) {}
+
+  template<typename Archive>
+  void Serialize(Archive& ar) {
+    ar(name_, age_);
+  }
+
+  std::string name_;
+  int age_;
+};
+
+TEST_CASE("class with object types can be serialized") {
+  std::stringstream ss;
+  draft::binary::OutputArchive oa(ss);
+  draft::binary::InputArchive ia(ss);
+
+  SECTION("encode simple Person instance") {
+    Person p1("Ada", 204);
+    oa(p1);
+    Person p2;
+    ia(p2);
+
+    REQUIRE(p2.name_ == "Ada");
+    REQUIRE(p2.age_ == 204);
   }
 }
