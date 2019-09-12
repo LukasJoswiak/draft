5d945dd6e6621ba182f22b8a511f1ab8446976ea
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-09-11 20:40:22 -0700

    Add std::unordered_multiset serialization support

--- include/types/unordered_set.hpp
+++ include/types/unordered_set.hpp
@@ -21,6 +21,17 @@ Save(OutputArchive&oa,
   std::for_each(set.begin(), set.end(), oa);
 }
 
+// TODO: Combine serialization for std::unordered_set and
+// std::unordered_multiset into a single function.
+// Add support for std::unordered_multiset as well.
+template<typename Key, typename Hash, typename KeyEqual, typename Allocator>
+void
+Save(OutputArchive&oa,
+    std::unordered_multiset<Key, Hash, KeyEqual, Allocator>& set) {
+  oa(set.size());
+  std::for_each(set.begin(), set.end(), oa);
+}
+
 // Deserialize the unordered set by reading the number of elements stored, then
 // reading each element.
 template<typename Key, typename Hash, typename KeyEqual, typename Allocator>
@@ -35,6 +46,23 @@ Load(binary::InputArchive& ia,
     ia(key);
 
     // TODO: This can likely be improved by using emplace_hint instead.
+    set.emplace(std::move(key));
+  }
+}
+
+// Support std::unordered_multiset deserialization.
+template<typename Key, typename Hash, typename KeyEqual, typename Allocator>
+void
+Load(binary::InputArchive& ia,
+    std::unordered_multiset<Key, Hash, KeyEqual, Allocator>& set) {
+  typename
+      std::unordered_multiset<Key, Hash, KeyEqual, Allocator>::size_type size;
+  ia(size);
+
+  for (std::size_t i = 0; i < size; ++i) {
+    Key key;
+    ia(key);
+
     set.emplace(std::move(key));
   }
 }
