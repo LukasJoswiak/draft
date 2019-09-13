953af1a6442d6818de8cd5074ceb7f299fcb886e
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-09-13 16:14:48 -0700

    Add serialization support for std::unordered_multimap

--- include/types/unordered_map.hpp
+++ include/types/unordered_map.hpp
@@ -30,6 +30,23 @@ Save(OutputArchive& oa,
   }
 }
 
+template<
+  typename K,
+  typename V,
+  typename Hash,
+  typename KeyEqual,
+  typename Allocator
+>
+void
+Save(OutputArchive& oa,
+    const std::unordered_multimap<K, V, Hash, KeyEqual, Allocator>& map) {
+  oa(map.size());
+  for (const auto& kv : map) {
+    oa(kv.first);
+    oa(kv.second);
+  }
+}
+
 // Deserialize the unordered_map by reading the number of elements stored,
 // then reading each key-value pair.
 template<
@@ -55,6 +72,29 @@ Load(binary::InputArchive& ia,
     // Let the map take control of `key` and `value` to avoid having to make
     // copies.
     // TODO: This can likely be improved by using emplace_hint instead.
+    map.emplace(std::move(key), std::move(value));
+  }
+}
+
+template<
+  typename K,
+  typename V,
+  typename Hash,
+  typename KeyEqual,
+  typename Allocator
+>
+void
+Load(binary::InputArchive& ia,
+    std::unordered_multimap<K, V, Hash, KeyEqual, Allocator>& map) {
+  std::size_t size;
+  ia(size);
+
+  for (std::size_t i = 0; i < size; ++i) {
+    K key;
+    V value;
+    ia(key);
+    ia(value);
+
     map.emplace(std::move(key), std::move(value));
   }
 }
