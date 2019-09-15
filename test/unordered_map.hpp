0aeb31ca126b0dd51aaec9e74c1db3e62e05aa21
Lukas Joswiak <lukas@lukasjoswiak.com>
2019-09-15 09:29:14 -0700

    Add std::unordered_multimap unit tests

--- test/unordered_map.hpp
+++ test/unordered_map.hpp
@@ -8,7 +8,7 @@ TEST_CASE("std::unordered_map can be serialized") {
   draft::binary::OutputArchive oa(ss);
   draft::binary::InputArchive ia(ss);
 
-  SECTION("encode map of fundamental types") {
+  SECTION("encode std::unordered_map of fundamental types") {
     std::unordered_map<int, double> m = {
       { 1, 3.14 },
       { 100, 9.99 }
@@ -17,13 +17,14 @@ TEST_CASE("std::unordered_map can be serialized") {
     std::unordered_map<int, double> n;
     ia(n);
 
+    REQUIRE(n.size() == 2);
     REQUIRE(n.count(1) == 1);
     REQUIRE(n.count(100) == 1);
     REQUIRE(n.at(1) == 3.14);
     REQUIRE(n.at(100) == 9.99);
   }
 
-  SECTION("encode map of objects") {
+  SECTION("encode std::unordered_map of objects") {
     std::unordered_map<int, std::string> m = {
       { 1, "hello" },
       { 2, "world" }
@@ -32,9 +33,46 @@ TEST_CASE("std::unordered_map can be serialized") {
     std::unordered_map<int, std::string> n;
     ia(n);
 
+    REQUIRE(n.size() == 2);
     REQUIRE(n.count(1) == 1);
     REQUIRE(n.count(2) == 1);
     REQUIRE(n.at(1) == "hello");
     REQUIRE(n.at(2) == "world");
+  }
+}
+
+TEST_CASE("std::unordered_multimap can be serialized") {
+  std::stringstream ss;
+  draft::binary::OutputArchive oa(ss);
+  draft::binary::InputArchive ia(ss);
+
+  SECTION("encode map of fundamental types") {
+    std::unordered_multimap<int, double> m = {
+      { 1, 3.14 },
+      { 100, 9.99 },
+      { 1, 600.5 }
+    };
+    oa(m);
+    std::unordered_multimap<int, double> n;
+    ia(n);
+
+    REQUIRE(n.size() == 3);
+    REQUIRE(n.count(1) == 2);
+    REQUIRE(n.count(100) == 1);
+  }
+
+  SECTION("encode std::unordered_multimap of objects") {
+    std::unordered_multimap<int, std::string> m = {
+      { 1, "hello" },
+      { 2, "world" },
+      { 2, "draft" }
+    };
+    oa(m);
+    std::unordered_multimap<int, std::string> n;
+    ia(n);
+
+    REQUIRE(n.size() == 3);
+    REQUIRE(n.count(1) == 1);
+    REQUIRE(n.count(2) == 2);
   }
 }
